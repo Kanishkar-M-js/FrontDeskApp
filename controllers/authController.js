452998e8d4c942305a99789396cf696e48dc3e89
Kanishkar-M-js <kanishkar4444@gmail.com>
2025-01-03 20:41:29 +0530

    setup controlers and models

---
+++ controllers/authController.js
@@ -0,0 +1,13 @@
+const { response, request } = require("express");
+
+const authController = {
+    register: async (request, response) => {
+        try {
+             response.json({ message: "Hello World!" });
+        } catch (error) {
+            response.status(500).json({ message: error.message });
+        }
+    }
+}
+
+module.exports = authController;
