597a179bbfd7d53c601eb4bfa5d9df129adab0a2
Kanishkar-M-js <kanishkar4444@gmail.com>
2025-01-06 19:02:49 +0530

    register and login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,57 @@
 const { response, request } = require("express");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../utils/config");
 
 const authController = {
-    register: async (request, response) => {
-        try {
-             response.json({ message: "Hello World!" });
-        } catch (error) {
-            response.status(500).json({ message: error.message });
-        }
+  register: async (request, response) => {
+    try {
+      // get the details from request body
+      const { name, email, password } = request.body;
+      // check if user already exists
+      const user = await User.findOne({ email });
+      if (user) {
+        return response.status(400).json({ message: "User already exists" });
+      }
+      // hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = new User({
+        name,
+        email,
+        password : hashedPassword,
+      });
+      await newUser.save();
+      response.status(201).json({ message: "User created successfully" });
+    } catch (error) {
+      response.status(500).json({ message: error.message });
     }
-}
+  },
+  login: async (request, response) => {
+    try {
+      // get the details from request body
+      const { email, password } = request.body;
+      // check if user already exists
+      const user = await User.findOne({ email });
+      if (!user) {
+        return response.status(400).json({ message: "User does not exist" });
+      }
+      // check if password is correct
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return response.status(400).json({ message: "Invalid credentials" });
+      }
+      // create a token
+      const token = jwt.sign({ id: user._id }, SECRET_KEY, {expiresIn: "3h"});
+      
+      response.cookie("token", token, { httpOnly: true });
+
+      response.status(200).json({ message: "Login successful"});
+
+    } catch (error) { 
+      response.status(500).json({ message: error.message });
+    }
+  },
+};
 
 module.exports = authController;
--- utils/config.js
+++ utils/config.js
@@ -2,5 +2,6 @@ require("dotenv").config();
 
 MONGODB_URI = process.env.MONGODB_URI;
 PORT = process.env.PORT;
+SECRET_KEY = process.env.SECRET_KEY;
 
-module.exports = { MONGODB_URI, PORT };
+module.exports = { MONGODB_URI, PORT, SECRET_KEY };
